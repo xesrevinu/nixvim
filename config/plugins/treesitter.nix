50d72a0874e94ab88e3315a54736a1b110486b39
Maxwell Brown <maxwellbrown1990@gmail.com>
2024-09-13 19:00:41 -0400

    install all treesitter grammars

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -3,28 +3,10 @@
     treesitter = {
       enable = true;
       nixvimInjections = true;
-      # There are additional nvim-treesitter modules that you can use to interact
-      # with nvim-treesitter. You should go explore a few and see what interests you:
-      #
-      #    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      #    - Show your current context: https://nix-community.github.io/nixvim/plugins/treesitter-context/index.html
-      #    - Treesitter + textobjects: https://nix-community.github.io/nixvim/plugins/treesitter-textobjects/index.html
       settings = {
         indent.enable = true;
         highlight.enable = true;
         incremental_selection.enable = true;
-        ensure_installed = [
-          "bash"
-          "css"
-          "html"
-          "javascript"
-          "json"
-          "lua"
-          "markdown"
-          "tsx"
-          "typescript"
-          "yaml"
-        ];
       };
     };
 
