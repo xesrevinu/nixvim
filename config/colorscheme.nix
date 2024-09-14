2f15d50184c8d9dab560813367a252c4c5c1c343
Maxwell Brown <maxwellbrown1990@gmail.com>
2024-09-13 20:41:17 -0400

    change colorscheme

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -3,6 +3,9 @@
     catppuccin = {
       enable = true;
       settings = {
+        flavour = "macchiato";
+        no_bold = true;
+        no_italic = true;
         integrations = {
           cmp = true;
           gitsigns = true;
