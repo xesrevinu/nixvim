1c550644e7ceb784b1258e9c4a601630532faad2
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-04-23 17:54:27 -0400

    ensure snippets are exited when leaving snippet

--- config/plugins/luasnip.nix
+++ config/plugins/luasnip.nix
@@ -10,4 +10,25 @@
       updateevents = "TextChanged,TextChangedI";
     };
   };
+
+  autoCmd = [
+    {
+      desc = "Leave snippet on mode change";
+      event = [ "ModeChanged" ];
+      callback = {
+        __raw =
+          # lua
+          ''
+            function()
+              if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+                   and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+                   and not require('luasnip').session.jump_active
+               then
+                 require('luasnip').unlink_current()
+               end
+             end
+          '';
+      };
+    }
+  ];
 }
