9046108fda99401e3740324f9644d7c093fcc254
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-04-26 10:47:13 -0400

    turn off virtual line

--- config/settings.nix
+++ config/settings.nix
@@ -9,9 +9,6 @@
 
     diagnostics = {
       virtual_text = false;
-      virtual_lines = {
-        current_line = true;
-      };
       signs = true;
       underline = true;
       update_in_insert = true;
