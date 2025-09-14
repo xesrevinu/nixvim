f99170ee553984b1411f27973c455d92e07bcf91
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-09-14 11:37:07 -0400

    add command to toggle format on save

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -1,4 +1,38 @@
 {
+  userCommands = {
+    "FormatEnable" = {
+      desc = "Enable format on save";
+      command = {
+        __raw =
+          # lua
+          ''
+            function()
+              vim.b.disable_autoformat = false
+              vim.g.disable_autoformat = false
+            end
+          '';
+      };
+    };
+    "FormatDisable" = {
+      desc = "Disable format on save";
+      bang = true;
+      command = {
+        __raw =
+          # lua
+          ''
+            function(args)
+              if args.bang then
+                -- FormatDisable! will disable formatting just for this buffer
+                vim.b.disable_autoformat = true
+              else
+                vim.g.disable_autoformat = true
+              end
+            end
+          '';
+      };
+    };
+  };
+
   plugins.conform-nvim = {
     enable = true;
     settings = {
@@ -6,10 +40,22 @@
       notify_on_error = false;
       notify_no_formatters = false;
       format_after_save = {
-        lsp_format = "prefer";
-        stop_after_first = true;
-        timeout_ms = 500;
-        async = true;
+        __raw =
+          # lua
+          ''
+            function(bufnr)
+              -- Disable with a global or buffer-local variable
+              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+              end
+              return {
+                lsp_format = "prefer",
+                stop_after_first = true,
+                timeout_ms = 500,
+                async = true,
+              }
+            end
+          '';
       };
       formatters_by_ft =
         let
