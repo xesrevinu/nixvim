24c3911f5d7ca6ac6b251131f9f39234cd8435cb
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-11-11 07:09:49 -0500

    upgrade deps and disable tailwind-tools

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -22,40 +22,42 @@
 
     treesitter-textobjects = {
       enable = true;
-      select = {
-        enable = true;
-        lookahead = true;
-        keymaps = {
-          "aa" = "@parameter.outer";
-          "ia" = "@parameter.inner";
-          "af" = "@function.outer";
-          "if" = "@function.inner";
-          "ac" = "@class.outer";
-          "ic" = "@class.inner";
-          "ii" = "@conditional.inner";
-          "ai" = "@conditional.outer";
-          "il" = "@loop.inner";
-          "al" = "@loop.outer";
-          "at" = "@comment.outer";
+      settings = {
+        select = {
+          enable = true;
+          lookahead = true;
+          keymaps = {
+            "aa" = "@parameter.outer";
+            "ia" = "@parameter.inner";
+            "af" = "@function.outer";
+            "if" = "@function.inner";
+            "ac" = "@class.outer";
+            "ic" = "@class.inner";
+            "ii" = "@conditional.inner";
+            "ai" = "@conditional.outer";
+            "il" = "@loop.inner";
+            "al" = "@loop.outer";
+            "at" = "@comment.outer";
+          };
         };
-      };
-      move = {
-        enable = true;
-        gotoNextStart = {
-          "]m" = "@function.outer";
-          "]]" = "@class.outer";
-        };
-        gotoNextEnd = {
-          "]M" = "@function.outer";
-          "][" = "@class.outer";
-        };
-        gotoPreviousStart = {
-          "[m" = "@function.outer";
-          "[[" = "@class.outer";
-        };
-        gotoPreviousEnd = {
-          "[M" = "@function.outer";
-          "[]" = "@class.outer";
+        move = {
+          enable = true;
+          gotoNextStart = {
+            "]m" = "@function.outer";
+            "]]" = "@class.outer";
+          };
+          gotoNextEnd = {
+            "]M" = "@function.outer";
+            "][" = "@class.outer";
+          };
+          gotoPreviousStart = {
+            "[m" = "@function.outer";
+            "[[" = "@class.outer";
+          };
+          gotoPreviousEnd = {
+            "[M" = "@function.outer";
+            "[]" = "@class.outer";
+          };
         };
       };
     };
